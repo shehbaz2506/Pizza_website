b45606b69d207f674e45e388b817496e6ef68c08
shehbaz2506 <shehbazsinghchauhan878@gmail.com>
2026-04-14 17:30:21 +0530

    Usage of Props

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -18,7 +17,7 @@ const pizzaData = [
     photoName: "pizzas/margherita.jpg",
     soldOut: false,
   },
-  {  
+  {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
@@ -48,67 +47,80 @@ const pizzaData = [
   },
 ];
 
-
-function App(){
-    return <div className="container">
-        <Header />
-        <Menu />
-        <Footer />
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
-}
-
-function Header(){
-   return (
-    <header className="header"> 
-       <h1>Fast React Pizza Co.</h1>
-    </header>
-   );
-}
-
-function Menu(){
-  return( <main className="menu">
-    <h2>Our Menu</h2>
-    <Pizza />
-    <Pizza />
-  </main>
   );
 }
 
-function Pizza(){
-    return <div>
-        <img src="pizzas/focaccia.jpg" alt="Pizza Spinaci" />
-        <h3>Pizza Spinaci</h3><p>Tomato, mozarella, spinach, and ricotta cheese</p>
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/focaccia.jpg"
+        price="10"
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price="10"
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div>
+      <img src={props.photoName} alt={props.name} />
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
     </div>
+  );
 }
 
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
 
-function Footer(){
+  // if(hour >= openHour && hour <=closeHour){
+  //   alert("We're Currently open.!")
+  // }
+  // else{
+  //   alert("Sorry we're Closed")
+  // }
 
-    const hour = new Date().getHours()
-    const openHour = 12;
-    const closeHour = 22; 
-    const isOpen = hour >= openHour && hour <=closeHour;
-    console.log(isOpen);
-
-    // if(hour >= openHour && hour <=closeHour){
-    //   alert("We're Currently open.!")
-    // }
-    // else{
-    //   alert("Sorry we're Closed")
-    // } 
-      
-    
-
-    return <footer className="footer">{new Date().toLocaleTimeString()}We're currently open</footer>
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}.We're currently open
+    </footer>
+  );
 }
 
-
-
-
-//react version 18 
+//react version 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
